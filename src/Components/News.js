f6e35ff4edeb92204595e58232399e3445f3590d
Sarang Sahebrao Gaidhani <saranggaidhani22@gmail.com>
2023-07-17 18:43:52 +0530

    Created a new app

---
+++ src/Components/News.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+
+export class News extends Component {
+  constructor() {
+    super();
+    console.log("Hello, I am a constructor from the News component");
+    this.state = {
+      articles: [],
+      loading: false,
+      page:1
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=f26520794a6949ab8ae9e7dfe66f17d1&page=1&pageSize=$(this.props.pageSize)`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles,totalResults: parsedData.totalResults});
+  }
+  
+  handlePrevClick = async() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=f26520794a6949ab8ae9e7dfe66f17d1&page=${this.state.page - 1}&pageSize=$(this.props.pageSize)`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+
+  }
+  
+  handleNextClick = async () => {
+    console.log("next");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=f26520794a6949ab8ae9e7dfe66f17d1&page=${this.state.page + 1}&pageSize=$(this.props.pageSize)`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      })
+    }
+  
+  }
+
+  render() {
+    return (
+      <div className='container my-3'>
+        <h1 className='text-center'>One Stop News - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles?.map((element) => (
+            <div className="col-md-4" key={element.url}>
+              <NewsItem
+                title={element.title?element.title.slice(0,45):" "}
+                description={element.description?element.description.slice(0,88):" "}
+                imageUrl={element.urlToImage}
+                newsUrl={element.url}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled = {this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled = {this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl?"https://img.etimg.com/thumb/msid-101695719,width-1070,height-580,imgsize-32230,overlay-etmarkets/photo.jpg":imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreference" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More...</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
